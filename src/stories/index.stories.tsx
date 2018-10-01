abf5d2322904ea138cbb132c80d1d3b79f968d61
Nick Mason <kiljadn@gmail.com>
2018-09-30 21:36:02 -0400

    Getting FramerX components in this

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -7,6 +7,7 @@ import { withInfo } from '@storybook/addon-info';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
@@ -20,6 +21,14 @@ storiesOf('Button', module)
   ));
 
 import { NickComponent } from '../components/NickComponent';
+import { FAB } from '../components/mdcButton';
+import { Inputs_Select } from '../components/mdcSelect';
+import { Inputs_Checkbox } from '../components/mdcCheckbox';
+import { Card } from '../components/mdcCard';
 
-storiesOf('TSX Components', module)
-  .add('nickComponent', (() => <NickComponent text={'Hi there.'} viewCount={10} />));
+storiesOf('FramerX components', module)
+  .add('nickComponent', (() => <NickComponent text={'Hi there.'} viewCount={10} />))
+  .add('FAB', (() => <FAB></FAB>))
+  .add('Material Select', (() => <Inputs_Select></Inputs_Select>))
+  .add('Material Checkbox', (() => <Inputs_Checkbox></Inputs_Checkbox>))
+  .add('Material Card', (() => <Card action1={' '} action2={' '}></Card>))
